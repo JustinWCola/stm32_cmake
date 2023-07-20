137c347b1e41350376f88bd145646b30511d9850
JustinWCola <867581856@qq.com>
2023-07-20 14:40:43 +0800

    Modern CMake init

---
+++ User/Library/LED/led.cpp
@@ -0,0 +1,5 @@
+//
+// Created by JustinWCola on 2023/7/19.
+//
+
+#include "led.h"
---
+++ User/Library/LED/led.h
@@ -0,0 +1,15 @@
+//
+// Created by JustinWCola on 2023/7/19.
+//
+
+#ifndef STM32_CMAKE_LED_H
+#define STM32_CMAKE_LED_H
+
+
+class led
+{
+
+};
+
+
+#endif //STM32_CMAKE_LED_H
